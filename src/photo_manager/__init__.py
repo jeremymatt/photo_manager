e0d71a804e5000b97478df8ad9c8c5bce001f0c9
jeremy matt <jeremy.matt@gmailcom>
2026-02-07 20:03:15 -0500

    start GPT branch

--- src/photo_manager/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Photo Manager - Cross-platform photo organizer and viewer."""
-
-__version__ = "0.1.0"
--- src/photo_manager/viewer/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Allow running the viewer as: python -m photo_manager.viewer"""
-
-import sys
-from photo_manager.viewer.app import main
-
-sys.exit(main())
